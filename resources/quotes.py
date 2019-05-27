b6d143dd904049022201b4e68a9feacade71d0b2
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-05-27 17:26:59 -0600

    more quotes, all routes working in postman

--- resources/quotes.py
+++ resources/quotes.py
@@ -4,17 +4,56 @@ import models
 
 quote_fields = {
     'id': fields.Integer,
-    'quote': fields.String,
+    'body': fields.String,
     'attributed_to': fields.String,
     'medium': fields.String
 }
+
+
+class QuoteList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'body',
+            required=False,
+            help='No quote provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'attributed_to',
+            required=False,
+            help='No author provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'medium',
+            required=False,
+            help='No medium provided',
+            location=['form', 'json']
+        )   
+        super().__init__()
+
+    @marshal_with(quote_fields)
+    def get(self):
+        new_quotes = [marshal(quote, quote_fields) for quote in models.Quote.select()]
+        return (new_quotes, 201)
+
+    @marshal_with(quote_fields)
+    def post(self):
+        args = self.reqparse.parse_args()
+        print(args)
+        quote = models.Quote.create(**args)
+        return (quote, 201)
+
+
+
 
 
 class Quote(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument(
-            'quote',
+            'body',
             required=False,
             help='No quote provided',
             location=['form', 'json']
@@ -32,6 +71,7 @@ class Quote(Resource):
             location=['form', 'json']
         )    
         super().__init__()
+
     @marshal_with(quote_fields)
     def get(self, id):
         try:
@@ -40,6 +80,7 @@ class Quote(Resource):
             abort(404)
         else:
             return (quote, 200)
+
     @marshal_with(quote_fields)
     def put(self, id):
         args = self.reqparse.parse_args()
@@ -49,7 +90,7 @@ class Quote(Resource):
         return (models.Quote.get(models.Quote.id==id), 200)
 
     def delete(self, id):
-        query = models.Quote.delete().where(models.Dog.id==id)
+        query = models.Quote.delete().where(models.Quote.id==id)
         query.execute()
         return {"message": "resource deleted"}
 
@@ -57,7 +98,11 @@ class Quote(Resource):
 quotes_api = Blueprint('resources.quotes', __name__)
 
 api = Api(quotes_api)
-
+api.add_resource(
+    QuoteList,
+    '/quotes',
+    endpoint='quotes'
+)
 api.add_resource(
     Quote,
     '/quotes/<int:id>',
